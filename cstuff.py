2cda6dc8356874acd0216187c833337c4b288017
ethanxu8 <xuethan2004@gmail.com>
2024-08-26 16:51:09 -0600

    nested loops and O(n^2) time complexity

--- cstuff.py
+++ cstuff.py
@@ -1,27 +1,36 @@
-print('hello world')
+# print('hello world')
 
-def multiply_numbers(n):
-    return n*n
+# def multiply_numbers(n):
+#     return n*n
 
-print(multiply_numbers(4))
+# print(multiply_numbers(4))
 
-# O(1) time complexity because the only function is n*n regardless of what number n is. 
+# # O(1) time complexity because the only function is n*n regardless of what number n is. 
 
 
 
-def range_values(n): 
-    for i in range(n):
-        print(i)
+# def range_values(n): 
+#     for i in range(n):
+#         print(i)
         
-print(range_values(10))
+# print(range_values(10))
 
-# O(n) time complexity because the number of functions increases linearly with the number n. 
+# # O(n) time complexity because the number of functions increases linearly with the number n. 
+
+# def range_values(n): 
+#     for i in range(n):
+#         print(i)
+#     for j in range(n):
+#         print(j)
+        
+# range_values(10)
+
+# # This is technically O(2n) time complexity because it is n + n. However, when n gets really large, 2 becomes negligible. Therefore when we have a constant like 2 we can just drop it. So O(2n) = O(n)
 
 def range_values(n): 
     for i in range(n):
-        print(i)
-    for j in range(n):
-        print(j)
+        for j in range(n):
+            print(i, j)
         
 range_values(10)
 
