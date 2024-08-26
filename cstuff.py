9ba9ab9a4dcfc6a2e2a517a971c6c65082a9efd3
ethanxu8 <xuethan2004@gmail.com>
2024-08-26 16:35:57 -0600

    drop constants

--- cstuff.py
+++ cstuff.py
@@ -16,3 +16,13 @@ def range_values(n):
 print(range_values(10))
 
 # O(n) time complexity because the number of functions increases linearly with the number n. 
+
+def range_values(n): 
+    for i in range(n):
+        print(i)
+    for j in range(n):
+        print(j)
+        
+range_values(10)
+
+# This is technically O(2n) time complexity because it is n + n. However, when n gets really large, 2 becomes negligible. Therefore when we have a constant like 2 we can just drop it. So O(2n) = O(n)
