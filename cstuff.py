afb29af66bc6a18c65ca5d6f08fafe5b301b6962
ethanxu8 <xuethan2004@gmail.com>
2024-08-26 15:56:17 -0600

    O(n) time complexity

--- cstuff.py
+++ cstuff.py
@@ -6,3 +6,13 @@ def multiply_numbers(n):
 print(multiply_numbers(4))
 
 # O(1) time complexity because the only function is n*n regardless of what number n is. 
+
+
+
+def range_values(n): 
+    for i in range(n):
+        print(i)
+        
+print(range_values(10))
+
+# O(n) time complexity because the number of functions increases linearly with the number n. 
