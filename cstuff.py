2ba5b653c75ae7208a8773ab3de76c70663f7f32
ethanxu8 <xuethan2004@gmail.com>
2024-08-26 15:37:09 -0600

    second commit

---
+++ cstuff.py
@@ -0,0 +1,8 @@
+print('hello world')
+
+def multiply_numbers(n):
+    return n*n
+
+print(multiply_numbers(4))
+
+# O(1) time complexity because the only function is n*n regardless of what number n is. 
